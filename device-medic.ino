3abe0a5ab3712b30809356b7d46647fdfebf0f69
Chris McCormick <chris@mccormick.cx>
2018-01-25 09:10:56 +0800

    Virtualized multiple knobs and buttons.

--- device-medic.ino
+++ device-medic.ino
@@ -18,12 +18,17 @@
 
 #define MIDI_CHANNEL 1
 
+#define RATEBUFFER 16
+#define NUM_VIRTUAL 6
+
 int frame = 0;
 
-// int values[2] = {0, 0};
-int values[2] = {0, 0};
-int values_previous[2] = {0, 0};
-int value_frame[2] = {0, 0};
+uint8_t values[2][RATEBUFFER];
+int change[2] = {0, 0};
+
+int value_virtual_knobs[6] = {0, 0, 0, 0, 0, 0};
+int value_virtual_button[6] = {0, 0, 0, 0, 0, 0};
+int selected = 0;
 
 int knob_buttons_source[2] = {PIN_F5, PIN_F4};
 int knob_buttons[2] = {0, 0};
@@ -40,13 +45,13 @@ Encoder encoders[2] = {
   Encoder(PIN_D2, PIN_D3)
 };
 
-int colors[6] = {
-  pixels.Color(0, 1, 0),
-  pixels.Color(0, 0, 1),
-  pixels.Color(0, 1, 1),
-  pixels.Color(1, 1, 0),
-  pixels.Color(1, 0, 0),
-  pixels.Color(1, 0, 1),
+uint32_t colors[6] = {
+  pixels.Color(1, 0, 2),
+  pixels.Color(0, 0, 2),
+  pixels.Color(0, 2, 0),
+  pixels.Color(2, 2, 0),
+  pixels.Color(2, 1, 0),
+  pixels.Color(2, 0, 0),
 };
 
 void setup() {
@@ -60,70 +65,77 @@ void setup() {
     pinMode(buttons_leds[i], OUTPUT);
   }
   
+  for (int i=0; i<2; i++) {
+    for (int r=0; r<RATEBUFFER; r++) {
+      values[i][r] = 0;
+    }
+  }
+  
   pixels.begin();
   
   // flash LED to indicate startup
   pinMode(LED, OUTPUT);
-  for (int i=0; i<6; i++) {  
+  for (int i=0; i<6; i++) {
     digitalWrite(LED, i % 2 ? HIGH : LOW);
     delay(250);
   }
+  
+  //Serial.begin(9600);
+  //Serial.println("Hello World...");
 }
 
 void loop() {
   for (int i=0; i<2; i++) {
-    values[i] = encoders[i].read();
-    // check knob values
-    if (values_previous[i] / 4 != values[i] / 4) {
-      // check for fast Without pushbutton code
-      int frames = frame - value_frame[i];
-      int diff = values[i] - values_previous[i];
-      int mult = 1;
-      if (frames < 100) {
-      	mult = 2;
-      }
-      if (frames < 50) {
-        mult = 4;
-      }
-      if (frames < 10) {
-        mult = 8;
-      }
-      values[i] = values_previous[i] + (diff * mult);
-      values[i] = values[i] < 0 ? 0 : values[i];
-      values[i] = values[i] > 511 ? 511: values[i];
-      value_frame[i] = frame;
-      // send new value
-      values_previous[i] = values[i];
-      usbMIDI.sendControlChange(i + 1, values_previous[i] / 4, MIDI_CHANNEL);
-      int offset = (!i) * PIXELS;
-      for (int l=0; l < PIXELS; l++) {
-        int remainder = min(max((values_previous[i]) / 4 - l * 8, 0), 8);
-        pixels.setPixelColor((PIXELS - 1 - l) + offset, remainder * colors[i]);
-      }
-      pixels.show();
+    // index into virtual knob/button tables
+    int lookup = (selected + i) % NUM_VIRTUAL;
+
+    // take a bead on each encoder
+    int raw = encoders[i].read();
+    int bead = raw / 4;
+    if (raw != change[i]) {
+      change[i] = raw;
+      usbMIDI.sendPitchBend(raw + 1024, MIDI_CHANNEL);
+    }
+    encoders[i].write(encoders[i].read() % 4);
+
+    values[i][frame % RATEBUFFER] = bead;
+    int rate = 0;
+    for (int r=0; r<RATEBUFFER; r++) {
+      rate = rate + min(abs(values[i][r]), 1);
     }
     
-    encoders[i].write(values[i]);
+    int new_knob_value = min(max(0, value_virtual_knobs[lookup] + bead * rate), 127);
+    if (new_knob_value != value_virtual_knobs[lookup]) {
+      value_virtual_knobs[lookup] = new_knob_value;
+      usbMIDI.sendControlChange(lookup, new_knob_value, MIDI_CHANNEL);
+    }
     
-    // check button values
+    int offset = (!i) * PIXELS;
+    for (int l=0; l < PIXELS; l++) {
+      int remainder = min(max(value_virtual_knobs[lookup] - l * 8, 0), 8);
+      pixels.setPixelColor((PIXELS - 1 - l) + offset, remainder * colors[lookup]);
+    }
+    pixels.show();
+    
+    // check knob button values
     knob_buttons[i] = digitalRead(knob_buttons_source[i]);
     if (knob_buttons_previous[i] != knob_buttons[i]) {
       knob_buttons_previous[i] = knob_buttons[i];
-      //usbMIDI.sendControlChange(2 + i, (1 - knob_buttons_previous[i]) * 127, MIDI_CHANNEL);
-    }
-  }
-  
-  for (int i=0; i<2; i++) {
-    int button_state = !digitalRead(buttons_source[i]);
-    if (button_state != buttons_previous[i]) {
-      // digitalWrite(buttons_leds[i], button_state);
-      if (button_state) {
-        usbMIDI.sendNoteOn(i + 1, 127, MIDI_CHANNEL);
-      } else {
-        usbMIDI.sendNoteOff(i + 1, 0, MIDI_CHANNEL);
+      if (knob_buttons[i]) {
+        selected = ((selected + (((!i) * 2) - 1)) + NUM_VIRTUAL) % NUM_VIRTUAL;
+        usbMIDI.sendControlChange(13, selected, MIDI_CHANNEL);
       }
-      buttons_previous[i] = button_state;
+      usbMIDI.sendControlChange(14 + (!i), (1 - knob_buttons[i]) * 127, MIDI_CHANNEL);
     }
+    
+    int button_state = !digitalRead(buttons_source[!i]);
+    if (button_state != buttons_previous[!i]) {
+      usbMIDI.sendControlChange(lookup + NUM_VIRTUAL, button_state * 127, MIDI_CHANNEL);
+      buttons_previous[!i] = button_state;
+    }
+
+    // update button LEDs
+    digitalWrite(buttons_leds[!i], min(1, value_virtual_button[lookup]));
   }
   
   // check if there is any incoming MIDI data
@@ -134,21 +146,23 @@ void loop() {
       if (type == 1) {
           int note = usbMIDI.getData1();
           int velocity = usbMIDI.getData2();
-          if (note == 1 || note == 2) {
+          /*if (note == 1 || note == 2) {
             digitalWrite(buttons_leds[note - 1], 1);
-	  }
+	  }*/
         } else if (type == 0) {
           int note = usbMIDI.getData1();
           int velocity = usbMIDI.getData2();
-          if (note == 1 || note == 2) {
+          /*if (note == 1 || note == 2) {
             digitalWrite(buttons_leds[note - 1], 0);
-	  }
+	  }*/
         } else if (type == 3) {
           int controller = usbMIDI.getData1();
           int value = usbMIDI.getData2();
-          if (controller == 0 || controller == 1) {
-            // TODO: set value
-	  }
+          if (controller >= 0 && controller < NUM_VIRTUAL) {
+            value_virtual_knobs[controller] = value;
+	  } else if (controller >= NUM_VIRTUAL && controller < NUM_VIRTUAL * 2 ) {
+	    value_virtual_button[controller - NUM_VIRTUAL] = value;
+          }
         } else {
           int d1 = usbMIDI.getData1();
 	  int d2 = usbMIDI.getData2();
