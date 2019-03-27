10ef17ab47435e6e39bbb298f1d7ac767dd0c69e
Chris McCormick <chris@mccormick.cx>
2019-03-27 10:23:50 +0800

    Update manufacturer name.

--- name.c
+++ name.c
@@ -1,19 +1,18 @@
-// To give your project a unique name, this code must be
-// placed into a .c file (its own tab).  It can not be in
-// a .cpp file or your main sketch (the .ino file).
+#include <usb_names.h>
 
-#include "usb_names.h"
+#define MANUFACTURER_NAME    {'b','z','z','t','.','s','t','u','d','i','o'}
+#define MANUFACTURER_NAME_LEN 11
+#define PRODUCT_NAME    {'t','h','u', 'm', 'b', 'e', 'l', 'i', 'n', 'a'}
+#define PRODUCT_NAME_LEN 10
 
-// Edit these lines to create your own name.  The length must
-// match the number of characters in your custom name.
-
-#define MIDI_NAME   {'t','h','u','m','b','e','l','i','n','a'}
-#define MIDI_NAME_LEN  10
-
-// Do not change this part.  This exact format is required by USB.
+struct usb_string_descriptor_struct usb_string_manufacturer_name = {
+        2 + MANUFACTURER_NAME_LEN * 2,
+        3,
+        MANUFACTURER_NAME
+};
 
 struct usb_string_descriptor_struct usb_string_product_name = {
-        2 + MIDI_NAME_LEN * 2,
+        2 + PRODUCT_NAME_LEN * 2,
         3,
-        MIDI_NAME
+        PRODUCT_NAME
 };
