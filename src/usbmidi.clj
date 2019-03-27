a638377389f0404c77abcda528191dea5acdeec9
Chris McCormick <chris@mccormick.cx>
2019-03-27 14:11:08 +0800

    Tidy up.

--- src/usbmidi.clj
+++ src/usbmidi.clj
@@ -2,8 +2,8 @@
 
 (defnative sendControlChange [controller value channel]
   (on true
-  ("MIDIUSB.h")
-  "usbMIDI.sendControlChange(number::to<number_t>(controller), number::to<number_t>(value), number::to<number_t>(channel));"))
+      "usbMIDI.sendControlChange(number::to<number_t>(controller), number::to<number_t>(value), number::to<number_t>(channel));"))
 
 (defnative sendSongPosition [spp]
-  "usbMIDI.sendSongPosition(number::to<number_t>(spp));")
+  (on true
+      "usbMIDI.sendSongPosition(number::to<number_t>(spp));"))
